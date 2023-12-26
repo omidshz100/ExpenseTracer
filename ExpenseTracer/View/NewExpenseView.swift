52a49540948b16928cb9121cee4dcfca95f480cf
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-12-26 18:09:30 +0100

    All codes for Adding or editing Data for expence traser has done

---
+++ ExpenseTracer/View/NewExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  NewExpenseView.swift
+//  ExpenseTracer
+//
+//  Created by Omid Shojaeian Zanjani on 26/12/23.
+//
+
+import SwiftUI
+
+struct NewExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewExpenseView()
+}
