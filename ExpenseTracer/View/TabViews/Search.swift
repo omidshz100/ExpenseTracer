d06070c180fcd084aea82722ffe52d39886ef803
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-12-26 16:11:42 +0100

    I've done until Episode - 3 , Completed setting and search page

--- ExpenseTracer/View/TabViews/Search.swift
+++ ExpenseTracer/View/TabViews/Search.swift
@@ -6,13 +6,41 @@
 //
 
 import SwiftUI
+import Combine
 
 struct Search: View {
+    // View Properties
+    @State private var searchText:String = ""
+    @State private var filterText:String = ""
+    let searchPublishe = PassthroughSubject<String, Never>()
     var body: some View {
-        Text("Search")
+        NavigationStack{
+            ScrollView(.vertical){
+                LazyVStack(spacing:12){
+                    
+                }
+            }
+            .overlay(content: {
+                ContentUnavailableView("Seach Transactions", systemImage: "magnifyingglass")
+                    .opacity(filterText.isEmpty ? 1:0)
+            })
+            .onChange(of: searchText, {oldVaue, newValue in
+                if newValue.isEmpty {
+                    filterText = ""
+                }
+                searchPublishe.send(newValue)
+            })
+            .onReceive(searchPublishe.debounce(for: .seconds(0.3) , scheduler: DispatchQueue.main), perform: { text in
+                filterText = text
+                print(text)
+            })
+            .searchable(text: $searchText)
+            .navigationTitle("Search")
+            .background(.gray.opacity(0.15))
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    Search()
 }
